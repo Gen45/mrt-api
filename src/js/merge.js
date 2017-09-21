83918e3b3fe661b814aeca885f42617dcca40c80
Genaro Desia Coppola <genarodesia@gmail.com>
2017-09-21 14:22:03 -0300

    removed comment

--- src/js/merge.js
+++ src/js/merge.js
@@ -12,12 +12,6 @@ console.log(noCala);
 
 let globalEvents = calaEvents.concat(noCala);
 
-// for (let event of events) {
-//     event["Brand"] = toAbr(splitBrands(event["Brand"]), brandsAbrev);
-//     // event["Brand"] = updateAbrv(event["Brand"]).toString();
-//     event["Brand"] = event["Brand"].toString();
-// }
-
 const content = JSON.stringify(globalEvents, null, 2);
 
 fs.writeFile("./data/exported/events.json", content, 'utf8', function (err) {
